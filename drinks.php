5392c8f0b4a627bb19cabcac8e4e40065ccb474d
CarolynMacaulay <110638692+CarolynMacaulay@users.noreply.github.com>
2022-08-17 12:44:00 +1200

    Add files via upload
    search for drink on the page

--- drinks.php
+++ drinks.php
@@ -24,6 +24,10 @@
     /* Fetch the result into an associative array */
     $this_drink_record = mysqli_fetch_assoc($this_drink_result);
 
+	/*Drinks query - SELECT drink id, item from drinks */
+	$all_drinks_query = "SELECT drink_id, drinks FROM drinks";
+	$all_drinks_result = mysqli_query($dbcon, $all_drinks_query);
+
 
 ?>
 <!DOCTYPE html>
@@ -44,6 +48,7 @@
 				<li><a href="index.php">Home</a></li>
 				<li><a href="drinks.php">Drinks</a></li>
 				<li><a href="orders.php">Orders</a></li>
+				<li><a href="customers.php">Customers</a></li>
 			</ul>
     	</nav>
         <h2>Drinks Information</h2>
@@ -51,5 +56,54 @@
             echo "<p>Drink Name: " .$this_drink_record['drinks']. "<br>";
             echo "Cost: $" .$this_drink_record['cost']. "</p>";
         ?>
+		
+		<h2>Select another drink</h2>
+		    <form name='drinks_form' id='drinks_form' method='get' action='drinks.php'>
+        <!--Dropdown menu-->
+        <select name='drink_sel' id='drink_sel'>
+            <!--Options-->
+
+            <?php
+            /* Display the query results into an option tag*/
+            while($all_drinks_record = mysqli_fetch_assoc($all_drinks_result)){
+				echo "<option value ='".$all_drinks_record['drink_id'] ."'>";
+                echo $all_drinks_record['drinks'];   // Show the drink name in the option box
+				echo "</option>";
+            }
+            ?>
+				</select>
+				 <input type="submit" name="orders_button" value="Show me another drink">
+		</form>
+		
+		<h2>Search for a drink</h2>
+		<form action="" method="post">
+			<input type="text" name="search">
+			<input type="submit" name="submit" value="Search">
+		</form>
+		
+		<?php
+		
+			if(isset($_POST['search'])) {
+				$search = $_POST['search'];
+
+				$query1 = "SELECT * FROM drinks WHERE drinks LIKE '%$search%'";
+				$query = mysqli_query($dbcon, $query1);
+				$count = mysqli_num_rows($query);
+
+				if($count == 0){
+					echo "There was no results";
+				}else{
+
+					while ($row = mysqli_fetch_array($query)) {
+						echo $row ['drinks'];
+						echo "<br>";
+						echo "$" .$row['cost'];
+						}
+				}
+				
+			}
+		?>
+			
+		
     </body>
 </html>
